ba8d7768dd273b77cf0edbd712e8fb6189658224
Leuris Castillo <leurisc123@gmail.com>
2020-06-02 19:46:18 -0400

    Initial commit

---
+++ Pedir dia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pedir_dia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia;
+            Console.WriteLine("Dame un dia: ");
+            dia = Console.ReadLine();
+
+            switch (dia)
+            {
+                case "lunes": Console.WriteLine("Haz elegido el lunes"); break;
+                case "martes": Console.WriteLine("Haz elegido el martes"); break;
+                case "miercoles": Console.WriteLine("Haz elegido el miercoles"); break;
+                case "jueves": Console.WriteLine("Haz elegido el jueves"); break;
+                case "viernes": Console.WriteLine("Haz elegido el viernes"); break;
+                case "sabado": Console.WriteLine("Haz elegido el sabado"); break;
+                case "domingo": Console.WriteLine("Haz elegido el domingo"); break;
+                default: Console.WriteLine("No es un dia"); break;
+
+            }
+        }
+    }
+}
